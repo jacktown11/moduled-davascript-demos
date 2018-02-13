75fcac5cc592ef120542bb653994a7ad8af1ce3a
jacktown11 <jikai_tang@163.com>
2018-02-13 08:54:57 +0800

    add commonjs example

---
+++ CommonJSExample/README.md
@@ -0,0 +1,3 @@
+# 使用方法
+- 在命令行(如windows环境下的cmd)中切换到该文件夹目录(如`<parentPathRoot>/CommonJSExample`)
+- 运行`node main`
---
+++ codeFragment.js
@@ -0,0 +1,13 @@
+// example.js
+var x = 5;
+var addX = function(value) {
+    return value + x;
+};
+module.exports.x = x;
+module.exports.addX = addX;
+
+// main.js
+var example = require('./example.js');
+
+console.log(example.x); // 5
+console.log(example.addX(1));   //6
